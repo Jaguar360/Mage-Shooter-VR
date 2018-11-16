f365e09dce86e5b55452ba5461cc5fe0bc50f191
Jaguar360 <jaguars310@msn.com>
2018-11-16 11:32:08 -0500

    Made staff summon Meteor projectile
    No interaction with enemy yet

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	private bool dead = false;
+	//public GameObject projectile;
+	private GameManager gameManager;
+
+	private void Awake()
+	{
+        gameManager = FindObjectOfType<GameManager>();      
+	}
+
+	//private void Start()
+	//{
+ //       StartCoroutine(Shoot());
+	//}
+
+	public void MakeDead(bool ded) 
+	{
+		dead = ded;
+	}
+
+	public bool IsDead() 
+	{
+		return dead;
+	}
+
+	private void Update()
+	{
+		if (dead == true) 
+		{
+			Destroy(gameObject);
+		}      
+	}
+    
+	//public IEnumerator Shoot()
+  //  {
+		//while (gameManager.IsGameOver() == false) 
+		//{
+  //          yield return new WaitForSeconds(3);
+  //          GameObject bullet = (GameObject)Instantiate(projectile);
+  //          bullet.transform.position = transform.position + transform.forward;
+  //          bullet.transform.forward = transform.forward;
+		//}
+    //}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+
+	void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void LateUpdate()
+	{
+		
+	}
+
+	public void Play() 
+	{   
+	}
+
+	public void GameOver(bool isover) 
+	{
+		//gameover = isover;	
+	}
+
+	//public bool IsGameOver() 
+	//{
+	//	return gameover;
+	//}
+
+    public void Restart()
+	{
+		//SceneManager.LoadScene(curScene.name);
+	}
+
+}
